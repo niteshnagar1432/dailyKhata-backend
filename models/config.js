28a08e221952adebc74af229ae417a2e61b13c4a
Nitesh Nagar <niteshnagar1142002@gmail.com>
2023-12-08 19:17:12 +0530

    db_uri

--- models/config.js
+++ models/config.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = 'mongodb://127.0.0.1:27017/dailyKhata';
+// const MONGODB_URI = `${process.env.MONGO_DB}dailyKhata`;
 
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-//   useCreateIndex: true,
-// });
+mongoose.connect(process.env.MONGO_DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 mongoose.connect(MONGODB_URI);
 
